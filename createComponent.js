7685126acc78e08d5d8e83adf9ad69c73ef98d2a
Yuhaigiub <buigiahuynt@gmail.com>
2023-07-11 10:56:18 +0700

    add htmlTemplate

--- createComponent.js
+++ createComponent.js
@@ -5,6 +5,23 @@ const workList = {
 	collection: ["component"],
 };
 
+const htmlTemplate = `{% spaceless %}
+{% set blockId = blockId|default("component_id") %}
+{% set blockIdPostFix = blockIdPostFix|default("") %}
+	<section id="{{ blockId }}{{ blockIdPostFix }}" class="section section--{{ blockId }} {{ blockId }} scrollFrame" data-block-id="{{ blockId }}">
+		<div class="section__background">
+			{# NOTE: add background images and video here #}
+			<span id="{{ blockId }}{{ blockIdPostFix }}-scrollwatch-pin" class="scrollwatch-pin"></span>
+		</div>
+		<div class="section__content">
+			<div class="inner inner--{{ blockId }}">
+				{# NOTE: add component's content here #}
+			</div>
+		</div>
+	</section>
+{% endspaceless %}
+`
+
 for (let [collection, components] of Object.entries(workList)) {
 	components.map((component) => {
 		createCollection(collection, component);
@@ -44,7 +61,9 @@ function createCollection(collectionId, componentId) {
 
 	["html.twig", "scss", "js"].map((ext) => {
 		const filePath = path.resolve(componentDir, `${componentId}.${ext}`);
-		fs.appendFile(filePath, "", function (err) {
+		let content = ""
+		if (ext === 'html.twig') content = htmlTemplate; 
+		fs.appendFile(filePath, content, function (err) {
 			if (err) console.log(err.message);
 		});
 	});
